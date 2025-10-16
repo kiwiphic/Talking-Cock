5deb29fd3b79813f9c6f4b31095b9bdc3fbbe33b
kiwiphic <neokieron@gmail.com>
2025-10-16 16:15:55 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -105,7 +105,7 @@ function draw() {
 
   if (playingShuffle) {
     // Show shuffle animation (PNG sequence)
-    image(shuffleFrames[currentFrame], cardX, cardY, (height*2) + 300, height*2);
+    image(shuffleFrames[currentFrame], cardX, cardY, height + 2400, height + 1500);
     // Adjust speed of animation (lower = faster)
     if (frameCount % 2 === 0) currentFrame++;
 
